f1782ceac2f9a7d21f7fc8e0912e85defffa2b91
cheun0327 <cheun0327@gmail.com>
2020-03-21 21:38:07 +0900

    SCSS

---
+++ jstest/test.js
@@ -0,0 +1,15 @@
+line = "500"
+line = parseInt(line);
+switch(line){
+    case 200:
+        console.log("웰치스");
+        break;
+    case 300:
+        console.log("코코팜");
+        break;
+    case 500:
+        console.log("아이스티");
+        break;
+    default:
+        console.log(line.toString());
+}
